8e48eef9172d0152ef799cb6c1b332c4faf8c6da
a.nvlkv <a.nvlkv@gmail.com>
2019-02-13 21:01:59 +0100

    moving to karma

--- src/containers/App.test.tsx
+++ src/containers/App.test.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
+import Page from 'react-page-object'
+import React from 'react'
+import { App } from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('AppSpec', () => {
+	let page;
+
+	beforeEach(() => {
+		page = new Page(<App />)
+	});
+
+	afterEach(() => {
+		page.destroy()
+	});
+
+	it('should pass', () => {
+		expect(page.content()).toMatch(/Welcome to React/)
+	});
 });
